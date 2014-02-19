83d2e0ffe004796e2395e14561a5d717362942b8
Sean Kelley-Pegg <sean@ackmanndickenson.com>
2014-02-19 11:43:40 -0600

    Added beforeSave and afterFind

--- app/Model/ProjectItem.php
+++ app/Model/ProjectItem.php
@@ -21,6 +21,27 @@ class ProjectItem extends AppModel {
  */
 	public $displayField = 'name';
 
+public function beforeSave($options = array())
+  {
+    // Encrypt the password for storage
+    if (isset($this->data[$this->alias]['password']) && !empty($this->data[$this->alias]['password']))
+    {
+        $newpass = Security::rijndael($this->data[$this->alias]['password'], Configure::read('Security.key'), 'encrypt');
+        $this->data[$this->alias]['password'] = $newpass;
+    }
+    return true;
+  }
+
+public function afterFind($results, $primary = false) {
+      // Decrypt the password for display
+      foreach ($results as $key => $val) {
+          if (isset($val['ProjectItem']['password'])) {
+              $results[$key]['ProjectItem']['password'] = Security::rijndael($results[$key]['ProjectItem']['password'], Configure::read('Security.key'), 'decrypt');
+          }
+      }
+    return $results;
+}
+
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
