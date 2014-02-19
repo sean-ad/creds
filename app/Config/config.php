689af99d20f4b48a2983652b83195a8d7d880fd8
Sean Kelley-Pegg <sean@ackmanndickenson.com>
2014-02-19 11:45:31 -0600

    Removing app/Config/config.php from version control

--- app/Config/config.php
+++ app/Config/config.php
@@ -21,6 +21,7 @@ $config = array(
 	// 	'from_email' => array('{{from_name}}' => '{{from_email}}'),
 	// 	'contact_mail' => array('{{contact_name}}' => '{{contact_mail}}')
 	// )
+	'Security' => array('key'=>'4bd55fd92337bc8ca28263bf2941ef815c32611be88887cd0a7e7997b6d5fc9d'),
 
 );
 
